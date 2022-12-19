a1847328b2b6a5abfa8a5051f02889bf8dfcdb0a
Alexander <100153293+GoldReserve@users.noreply.github.com>
2022-12-19 13:16:53 +0300

    Create image_to_image.py
    File name must be named exactly image_to_image

---
+++ image_to_image.py
@@ -0,0 +1,48 @@
+# For correct work need install https://imagemagick.org/script/download.php#windows
+# And if we need 1 exe file we can use terminal
+# pip install pyinstaller
+# pyinstaller --onefile image_to_image.py
+
+
+import os
+import subprocess
+from optparse import OptionParser
+from sys import platform
+
+directory = '.'
+
+
+def convert_formats_to_formats(input, output):
+    input_format_string = "." + input.strip().replace('.', '')
+    output_format_string = "." + output.strip().replace('.', '')
+
+    if not os.path.exists("output"):
+        os.mkdir("output")
+
+    for filename in os.listdir(directory):
+        if not filename.lower().endswith(input_format_string):
+            continue
+
+        print('Converting %s...' % os.path.join(directory, filename))
+
+        magick_command = ""
+        if platform == "linux" or platform == "linux2":
+            magick_command = "magick"
+        elif platform == "darwin":
+            magick_command = "magick"
+        elif platform == "win32":
+            magick_command = "magick.exe"
+
+        subprocess.run([magick_command, "%s" % filename, "output/%s" % (filename[0:-5] + output_format_string)])
+        continue
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-i", "--input", action="store", dest="input_format",
+                      help="Specify conversion format. Default is heic", default="heic")
+    parser.add_option("-o", "--output", action="store", dest="output_format",
+                      help="Specify conversion format. Default is jpg", default="jpg")
+    (options, args) = parser.parse_args()
+
+    convert_formats_to_formats(input=options.input_format, output=options.output_format)
